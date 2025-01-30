d0c84ddcc51d919b90ad2511ea15610247c14972
MathewBravo <mathewbravo94@gmail.com>
2025-01-29 23:14:36 -0500

    Now on lexical errors

--- src/scanner.zig
+++ src/scanner.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const t = @import("token.zig");
+const tt = @import("tokentype.zig");
 
 const Scanner = struct {
     source: []const u8,
@@ -38,5 +39,42 @@ const Scanner = struct {
 
     fn isAtEnd(self: *Scanner) bool {
         return self.current >= self.source.len;
+    }
+
+    fn scanToken(self: *Scanner) void {
+        const c = self.advance();
+        switch (c) {
+            '(' => self.addTokenNoLiteral(tt.TokenType.left_paren),
+            ')' => self.addTokenNoLiteral(tt.TokenType.right_paren),
+            '{' => self.addTokenNoLiteral(tt.TokenType.left_brace),
+            '}' => self.addTokenNoLiteral(tt.TokenType.right_brace),
+            ',' => self.addTokenNoLiteral(tt.TokenType.comma),
+            '.' => self.addTokenNoLiteral(tt.TokenType.dot),
+            '-' => self.addTokenNoLiteral(tt.TokenType.minus),
+            '+' => self.addTokenNoLiteral(tt.TokenType.plus),
+            ';' => self.addTokenNoLiteral(tt.TokenType.semicolon),
+            '*' => self.addTokenNoLiteral(tt.TokenType.star),
+            else => {},
+        }
+    }
+
+    fn advance(self: *Scanner) u8 {
+        const char_at = self.source[self.current];
+        self.current += 1;
+        return char_at;
+    }
+
+    fn addTokenNoLiteral(t_type: tt.TokenType) void {
+        addToken(t_type, t.LiteralValue{ .nil = {} });
+    }
+
+    fn addToken(self: *Scanner, t_type: tt.TokenType, literal: t.LiteralValue) void {
+        const text = self.source[self.start..self.current];
+        self.tokens.append(t.Token{
+            .type = t_type,
+            .lexeme = text,
+            .literal = literal,
+            .line = self.line,
+        });
     }
 };
